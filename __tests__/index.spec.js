f4c723210b9158dcbe91448f3ef0db34ad44f924
Jordan Sinko <jordan.sinko@amway.com>
2018-10-05 15:25:29 -0400

    removing async from tests

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,21 +1,23 @@
 const test = require('tape');
 const destructurify = require('..');
 
-test('destructurify handles resolved promises', async assert => {
+test('destructurify handles resolved promises', assert => {
+  assert.plan(2);
   const promiseFn = arg => new Promise(res => setTimeout(res, 0, arg));
   const fn = destructurify(promiseFn);
-  const { error, response } = await fn('test');
-  assert.equals(error, null);
-  assert.equals(response, 'test');
-  assert.end();
+  fn('test').then(({ error, response }) => {
+    assert.equals(error, null);
+    assert.equals(response, 'test');
+  });
 });
 
-test('destructurify handles rejected promises', async assert => {
+test('destructurify handles rejected promises', assert => {
+  assert.plan(2);
   const promiseFn = () =>
     new Promise((_, rej) => setTimeout(rej, 0, new Error('This is an error')));
   const fn = destructurify(promiseFn);
-  const { error, response } = await fn('test');
-  assert.equals(error.message, 'This is an error');
-  assert.equals(response, null);
-  assert.end();
+  fn('test').then(({ error, response }) => {
+    assert.equals(error.message, 'This is an error');
+    assert.equals(response, null);
+  });
 });
